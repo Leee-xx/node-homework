913b6c5c62ec96390247f09a020d43761913f346
Lee Wang <leeeee@gmail.com>
2026-07-07 15:37:12 -0700

    adds express script

--- assignment2/sampleHTTP.js
+++ assignment2/sampleHTTP.js
@@ -1,4 +1,24 @@
-const http = require('http')
+const http = require("http");
+
+const htmlString = `
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Clock</h1>
+<button id="getTimeBtn">Get the Time</button>
+<p id="time"></p>
+<script>
+document.getElementById('getTimeBtn').addEventListener('click', async () => {
+  const res = await fetch('/time');
+  const timeObj = await res.json();
+  console.log(timeObj);
+  const timeP = document.getElementById('time');
+  timeP.textContent = timeObj.time;
+});
+</script>
+</body>
+</html>
+`;
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/time") {
@@ -8,15 +28,35 @@ const server = http.createServer((req, res) => {
         time: new Date().toString(),
       }),
     );
-    return;
-  }
+  } else if (req.method === "GET" && req.url === "/timePage") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(htmlString);
+  } else if (req.method === "POST" && req.url === "/echo") {
+    let body = "";
 
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      message: "That route is not available.",
-    }),
-  );
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const parsedBody = JSON.parse(body);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          weReceived: parsedBody,
+        }),
+      );
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "That route is not available.",
+      }),
+    );
+  }
 });
 
 server.listen(8000);
+
