a3065dc16ab9dcca260360512bb87e10e1e37cd3
Lee Wang <leeeee@gmail.com>
2026-07-15 14:00:00 -0700

    creates and implements userController

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+function register(req, res) {
+  const user = {
+    id: 1,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }
+
+  global.users.push(user)
+  global.user_id = user.id
+
+  res.status(201).json({ name: user.name, email: user.email })
+}
+
+function logon(req, res) {
+  const { email, password } = req.body
+
+  const user = global.users.find((u) => u.email == email && u.password == password)
+
+  if (user) {
+    global.user_id = user.id
+    res.status(200).json({ name: user.name, email: user.email })
+  } else {
+    res.status(401)
+  }
+}
+
+function logoff(req, res) {
+  global.user_id = null
+
+  res.status(200)
+}
+
+module.exports = { register, logon, logoff }
