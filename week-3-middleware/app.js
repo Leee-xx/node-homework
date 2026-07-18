b648382b8ae86cbd33e75c2256675eb70adda49d
Lee Wang <leeeee@gmail.com>
2026-07-18 14:27:56 -0700

    wip

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -7,7 +7,7 @@ const { randomUUID } = require('crypto')
 const app = express();
 
 // Assignment 3b and 3c ask you to add middleware in this file.
-app.use(express.json())
+app.use(express.json({ limit: '1mb' }))
 app.use(
   express.static(path.join(__dirname, 'public'))
 )
@@ -21,6 +21,28 @@ app.use((req, res, next) => {
   console.log(`[${timestamp}]: ${req.method} ${req.path} (${req.requestId})`)
   next()
 })
+app.use((req, res, next) => {
+  console.log(`REQ METHD: ${req.method}`)
+  console.log(`REQ content type is json: ${req.is('application/json')}`)
+  if (req.method === 'POST' && !req.is('application/json')) {
+    console.log("NOT JSON")
+    return res.status(400).json({
+      message: 'Content-Type must be application/json',
+      requestId: req.requestId,
+    })
+  }
+
+  console.log("SHOULDN'T SEE THIS")
+  next()
+})
+
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'DENY')
+  res.setHeader('X-XSS-Protection', '1; mode=block')
+
+  next()
+})
 
 app.use("/", dogsRouter);// Do not remove this line
 
@@ -30,12 +52,22 @@ app.use((req, res, next) => {
     error: 'Route not found',
     requestId: req.requestId,
   })
-  next()
 })
 
+// Error handler
 app.use((err, req, res, next) => {
-  res.status(500).json({
-    error: 'Internal Server Error',
+  console.log('ERROR HANDLER')
+  const status = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+
+  if (status >= 400 && status < 500) {
+    console.warn(`WARN: ${err.name} - ${message}`)
+  } else {
+    console.error(`ERROR: ${err.name} - ${message}`)
+  }
+
+  res.status(status).json({
+    error: message,
     requestId: req.requestId,
   })
 })
